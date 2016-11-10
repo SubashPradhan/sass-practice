980df056d7ef0e8b4e4a3f1372bd82cc73be1c79
Subash Pradhan <suprowilde5@gmail.com>
2016-11-11 00:40:43 +0100

    final touches

---
+++ js/index-js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){/*
+	/*$(window).scroll(function () {
+			if ($(this).scrollTop() > 800) {
+				$('.navbar').fadeIn('slow');
+			} else {
+				$('.navbar').fadeOut('slow');
+			}
+		});
+});	
